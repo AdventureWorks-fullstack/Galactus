c14ab31f23af1994b0be3dfff53595e96c655da1
Arvid Ståhl Strömberg <arvostahl@gmail.com>
2021-07-02 21:06:19 +0200

    Add project files.

---
+++ Galactus.Schema/Query.cs
@@ -0,0 +1,85 @@
+﻿using AdventureWorks.Domain;
+using AdventureWorks.Domain.Models;
+using HotChocolate;
+using HotChocolate.Data;
+using HotChocolate.Types;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Galactus.Schema
+{
+    public class Query
+    {
+        [Serial]
+        [UsePaging]
+        [UseProjection]
+        [UseFiltering]
+        [UseSorting]
+        public IQueryable<Customer> GetCustomers([Service] AdventureWorksContext context) =>
+            context.Customers;
+
+        [Serial]
+        [UsePaging]
+        [UseProjection]
+        [UseFiltering]
+        [UseSorting]
+        public IQueryable<Department> GetDepartments([Service] AdventureWorksContext context) =>
+            context.Departments;
+
+        [Serial]
+        [UsePaging]
+        [UseProjection]
+        [UseFiltering]
+        [UseSorting]
+        public IQueryable<Employee> GetEmployees([Service] AdventureWorksContext context) =>
+            context.Employees;
+
+        [Serial]
+        [UsePaging]
+        [UseProjection]
+        [UseFiltering]
+        [UseSorting]
+        public IQueryable<Product> GetProducts([Service] AdventureWorksContext context) =>
+            context.Products;
+
+        [Serial]
+        [UsePaging]
+        [UseProjection]
+        [UseFiltering]
+        [UseSorting]
+        public IQueryable<PurchaseOrderHeader> GetPurchases([Service] AdventureWorksContext context) =>
+            context.PurchaseOrderHeaders;
+
+        [Serial]
+        [UseProjection]
+        public IQueryable<SalesOrderHeader> GetSales([Service] AdventureWorksContext context) =>
+            context.SalesOrderHeaders;
+
+        [Serial]
+        [UseProjection]
+        public IQueryable<ShoppingCartItem> GetShoppingCartById([Service] AdventureWorksContext context, string shoppingCartId) =>
+            context.ShoppingCartItems.Where(x => x.ShoppingCartId == shoppingCartId);
+
+        // TODO temp crap
+        [Serial]
+        public Task<List<string>> GetShoppingCarts([Service] AdventureWorksContext context) =>
+            context.ShoppingCartItems.Select(x => x.ShoppingCartId).ToListAsync();
+
+        [Serial]
+        [UseProjection]
+        public IQueryable<SpecialOffer> GetSpecials([Service] AdventureWorksContext context) =>
+            context.SpecialOffers;
+
+        [Serial]
+        [UsePaging]
+        [UseProjection]
+        [UseFiltering]
+        [UseSorting]
+        public IQueryable<WorkOrder> GetWorkOrders([Service] AdventureWorksContext context) =>
+            context.WorkOrders;
+    }
+}
---
+++ Galactus/Startup.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using AdventureWorks.Domain;
+using Microsoft.EntityFrameworkCore;
+using Galactus.Schema;
+
+namespace Galactus
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<AdventureWorksContext>(options => options.UseSqlServer("Server=localhost,1433;Database=AdventureWorks;User Id=SA;Password=my_password"));
+
+            services
+                .AddGraphQLServer()
+                .AddQueryType<Query>()
+                .AddProjections()
+                .AddFiltering()
+                .AddSorting();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+            }
+
+            app.UseDeveloperExceptionPage();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGraphQL("/");
+            });
+        }
+    }
+}
